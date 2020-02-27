5d54ae6eef40da25bd044fd848cedb4a942604ec
jomz <hi@monkeypatch.be>
2020-02-27 16:16:22 +0100

    Revert "Update to use Apache Sendfile header"
    This reverts commit a4cd91bed46edce568cce9a59f065a8f3686a504.
    Moving to new server env using nginx..

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -5,7 +5,7 @@ class DownloadsController < ReaderActionController
   def show
     @download = Download.find(params[:id])
     raise ReaderError::AccessDenied, t("downloads_extension.permission_denied") unless @download.visible_to?(current_reader)
-    response.headers['X-Sendfile'] = @download.document.path
+    response.headers['X-Accel-Redirect'] = @download.document.url
     response.headers["Content-Type"] = @download.document_content_type
     response.headers['Content-Disposition'] = "attachment; filename=#{@download.document_file_name}" 
     response.headers['Content-Length'] = @download.document_file_size
