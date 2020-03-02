9a5bb38b1dd9d786f2f92e9901cab1b809a409ce
jomz <hi@monkeypatch.be>
2020-03-02 18:49:28 +0100

    Set full path again for X-Accel-Redirect

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,17 +1,15 @@
 class DownloadsController < ReaderActionController
-  
+
   before_filter :require_reader, :only => [:show]
-  
+
   def show
     @download = Download.find(params[:id])
     raise ReaderError::AccessDenied, t("downloads_extension.permission_denied") unless @download.visible_to?(current_reader)
-    response.headers['X-Accel-Redirect'] = @download.document.url
+    response.headers['X-Accel-Redirect'] = @download.document.path
     response.headers["Content-Type"] = @download.document_content_type
-    response.headers['Content-Disposition'] = "attachment; filename=#{@download.document_file_name}" 
+    response.headers['Content-Disposition'] = "attachment; filename=#{@download.document_file_name}"
     response.headers['Content-Length'] = @download.document_file_size
     render :nothing => true
   end
-  
+
 end
-
-
